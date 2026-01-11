8025dfd31c8541e55df4640edb135251c88e8d0e
Pavlus Chaban <pavluschaban@MacBook-Pro-Olena.local>
2026-01-11 15:03:33 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,16 @@
-# write your code here
+def copy_file(config: str) -> None:
+    try:
+        command, fn_in, fn_out = config.split(" ")
+        if fn_in == fn_out or command != "cp" or fn_out is None:
+            return
+
+        try:
+            with (
+                open(fn_in, "r") as file_in,
+                open(fn_out, "w") as file_out,
+            ):
+                file_out.write(file_in.read())
+        except FileNotFoundError:
+            print(f"File {fn_in} not found")
+    except ValueError:
+        print("Invalid config")
